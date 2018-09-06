e7b8c2722aa61ca5069235b7aaca2432d8b2caf5
Aaron Madlon-Kay <amake@macports.org>
2018-09-06 10:08:21 +0900

    WIP: golang portgroup

--- _resources/port1.0/group/golang-1.0.tcl
+++ _resources/port1.0/group/golang-1.0.tcl
@@ -1,12 +1,20 @@
 # -*- coding: utf-8; mode: tcl; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- vim:fenc=utf-8:ft=tcl:et:sw=4:ts=4:sts=4
 #
 # This PortGroup accommodates golang projects hosted at GitHub.
-# Assumes use with github-1.0 PortGroup
+# Assumes use with github-1.0 PortGroup.
 
 options go.vendors
 
 options go.binary_name
-default go.binary_name {${name}}
+default go.binary_name  {${name}}
+
+default use_configure   no
+
+default depends_build   port:go
+
+default build.cmd       go
+default build.target    build
+default build.env       {"GOPATH=${gopath} CC=${configure.cc}"}
 
 # go.vendors-append name1 ver1 name2 ver2...
 # When a glide.lock is present:
@@ -22,25 +30,27 @@ proc go.vendors-append {args} {
         set vuser [lindex ${vlist} 1]
         set vname [lindex ${vlist} 2]
 
-        # NOTE: now expects github.com or golang.org
         switch -exact ${vdomain} {
             github.com { set ghuser ${vuser} }
             golang.org { set ghuser golang }
-            gopkg.in { if {$vname eq ""} {
-                set vname [regsub -- \\..*$ ${vuser} ""]
-                set ghuser go-${vname}
-            } else {
-                set vname [regsub -- \\..*$ ${vname} ""]
-                set ghuser ${vuser}
-            }}
+            gopkg.in {
+                if {$vname eq ""} {
+                    set vname [regsub -- \\..*$ ${vuser} ""]
+                    set ghuser go-${vname}
+                } else {
+                    set vname [regsub -- \\..*$ ${vname} ""]
+                    set ghuser ${vuser}
+                }
+            }
         }
 
-        set fname ${ghuser}-${vname}
-        lappend go.vendors [list ${fname} ${imp_name} ${vers}]
+        set sha1_short [string range ${vers} 0 6]
+        lappend go.vendors [list ${sha1_short} ${imp_name} ${vers}]
 
         global ${vname}.version
         set ${vname}.version ${vers}
 
+        set fname ${ghuser}-${vname}
         master_sites-append https://github.com/${ghuser}/${vname}/tarball/${vers}:${fname}
         distfiles-append    ${fname}-${vers}.tar.gz:${fname}
     }
@@ -55,21 +65,16 @@ proc go.vendors-append {args} {
 #             :
 set gopath      ${workpath}/gopath
 post-extract {
-    file mkdir ${gopath}/src/github.com/peco
+    file mkdir ${gopath}/src/github.com/${github.author}
     ln -s ${worksrcpath} ${gopath}/src/github.com/${github.author}/${github.project}
 
     foreach vlist ${go.vendors} {
-        set fname [lindex ${vlist} 0]
+        set sha1_short [lindex ${vlist} 0]
         set imp_name [lindex ${vlist} 1]
         file mkdir ${gopath}/src/[file dirname ${imp_name}]
-        move [glob ${workpath}/${fname}-*] ${gopath}/src/${imp_name}
+        move [glob ${workpath}/*-${sha1_short}] ${gopath}/src/${imp_name}
     }
 }
-
-default build.cmd       go
-default build.target    build
-default build.env       {GOPATH="${gopath}" \
-                             CC=${configure.cc}}
 
 destroot {
     xinstall -m 755 ${worksrcpath}/${go.binary_name} ${destroot}${prefix}/bin
